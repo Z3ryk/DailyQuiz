d5609fa716efff6759fa68ec23e7215d664d3881
Z3ryk <yanenko.2000@mail.ru>
2025-08-02 17:01:48 +0300

    feat: use router for navigation

--- DailyQuiz/DailyQuizApp.swift
+++ DailyQuiz/DailyQuizApp.swift
@@ -9,9 +9,14 @@ import SwiftUI
 
 @main
 struct DailyQuizApp: App {
+    @StateObject private var router = AppRouter()
+
     var body: some Scene {
         WindowGroup {
-            StartQuizView(viewModel: StartQuizViewModel())
+            NavigationContainer {
+                StartQuizView(viewModel: StartQuizViewModel(router: router))
+            }
+            .environmentObject(router)
         }
     }
 }
